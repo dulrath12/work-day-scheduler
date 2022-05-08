5a9a07f66c105525d66d08c8ba62d622dff42946
Dakotah Ulrath <dulrath@live.com>
2022-05-08 16:17:51 -0400

    timeblock color change

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,17 @@ var currentTime = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentTime);
 
 
+var timeBlock = $(".hour");
+var now = parseInt(moment().format("H"));
+
+$.each(timeBlock, function (i, hour) {
+  var hourId = parseInt($(this).attr("id"));
+  if (hourId === now) {
+    $(this).next().addClass("present");
+  } else if (hourId < now) {
+    $(this).next().addClass("past");
+  } else if (hourId > now) {
+    $(this).next().addClass("future");
+  }
+});
+
